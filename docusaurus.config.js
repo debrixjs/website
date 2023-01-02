9d5c7222886b8c2bb20a4e788ce1faa53db005c4
Elias Skogevall <elias.skogevall@gmail.com>
2023-01-02 17:25:30 +0000

    fix: invalid edit links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,11 +60,11 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          editUrl: "https://github.com/debrixjs/website/tree/main/docs",
+          editUrl: "https://github.com/debrixjs/website/tree/main",
         },
         blog: {
           showReadingTime: true,
-          editUrl: "https://github.com/debrixjs/website/tree/main/blog",
+          editUrl: "https://github.com/debrixjs/website/tree/main",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
